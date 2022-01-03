187530bebbe713a7c29a321a2c85e2658993e177
Ben <benjamind10@pm.me>
2022-01-03 12:47:13 -0500

    added form and search by user option

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+
 var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = 'https://api.github.com/users/' + user + '/repos';
@@ -9,3 +12,20 @@ var getUserRepos = function (user) {
     });
   });
 };
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  console.log(event);
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = '';
+  } else {
+    alert('Please enter a GitHub username');
+  }
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler);
